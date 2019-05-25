77c9d456172bda7e5a34454f59a7f8f4d7e79339
CJTSAJ <997149731@qq.com>
2019-05-25 11:23:51 +0800

    part1

--- src/main/java/sjtu/sdic/mapreduce/core/Reducer.java
+++ src/main/java/sjtu/sdic/mapreduce/core/Reducer.java
@@ -5,14 +5,13 @@ import com.alibaba.fastjson.JSONObject;
 import sjtu.sdic.mapreduce.common.KeyValue;
 import sjtu.sdic.mapreduce.common.Utils;
 
+import java.io.FileOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.nio.file.Paths;
+import java.util.*;
 
 /**
  * Created by Cachhe on 2019/4/19.
@@ -58,6 +57,66 @@ public class Reducer {
      * @param reduceFunc user-defined reduce function
      */
     public static void doReduce(String jobName, int reduceTask, String outFile, int nMap, ReduceFunc reduceFunc) {
-        
+        //read all domap file for the task and put them in a list
+        List<KeyValue> task_contents = new ArrayList<>();
+        for(int i = 0; i < nMap; i++){
+            //get the file name, and read the content
+            String file_name = Utils.reduceName(jobName, i, reduceTask);
+            String tmp_str = "";
+            try{
+                tmp_str = new String(Files.readAllBytes(Paths.get(file_name)));
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+
+            List<KeyValue> part_list = JSONArray.parseArray(tmp_str, KeyValue.class);
+            task_contents.addAll(part_list);
+        }
+
+        //sorting by key is helpful
+        Collections.sort(task_contents, new Comparator<KeyValue>() {
+            @Override
+            public int compare(KeyValue o1, KeyValue o2) {
+                return o1.key.compareTo(o2.key);
+            }
+        });
+
+        //merge values that have the same key
+        Map<String, List<String>> merge_content = new HashMap<>();
+        for(int i = 0; i < task_contents.size(); i++){
+            if(merge_content.get(task_contents.get(i).key) == null){
+                List<String> tmp_list = new ArrayList<>();
+                tmp_list.add(task_contents.get(i).value);
+                merge_content.put(task_contents.get(i).key, tmp_list);
+            }
+            else{
+                List<String> tmp_list = merge_content.get(task_contents.get(i).key);
+                tmp_list.add(task_contents.get(i).value);
+            }
+        }
+
+        //call reduceFunc
+        JSONObject result = new JSONObject();
+        for(String tmp_key : merge_content.keySet()){
+            List<String> tmp_list = merge_content.get(tmp_key);
+            String[] string_arr = tmp_list.toArray(new String[tmp_list.size()]);
+
+            String reduce_str = reduceFunc.reduce(tmp_key, string_arr);
+            result.put(tmp_key, reduce_str);
+        }
+
+        //write json result to file
+        File file = new File(outFile);
+        FileOutputStream outStr = null;
+        try {
+            outStr = new FileOutputStream(file);
+            String tmp_str = result.toJSONString();
+            outStr.write(tmp_str.getBytes());
+            outStr.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
