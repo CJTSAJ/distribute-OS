a96f7ba1d913b6ea93b89f457c38f765a544226c
CJTSAJ <997149731@qq.com>
2019-05-27 16:08:45 +0800

    part3

--- src/main/java/sjtu/sdic/mapreduce/core/Scheduler.java
+++ src/main/java/sjtu/sdic/mapreduce/core/Scheduler.java
@@ -1,15 +1,19 @@
 package sjtu.sdic.mapreduce.core;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import sjtu.sdic.mapreduce.common.Channel;
 import sjtu.sdic.mapreduce.common.DoTaskArgs;
 import sjtu.sdic.mapreduce.common.JobPhase;
 import sjtu.sdic.mapreduce.common.Utils;
 import sjtu.sdic.mapreduce.rpc.Call;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Vector;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by Cachhe on 2019/4/22.
@@ -51,9 +55,66 @@ public class Scheduler {
         // All ntasks tasks have to be scheduled on workers. Once all tasks
         // have completed successfully, schedule() should return.
         // Your code here (Part III, Part IV).
+        CountDownLatch downLatch = new CountDownLatch(nTasks);
 
-        
+        //vector: multi-thread safe
+        CopyOnWriteArrayList<Boolean> task_flag = new CopyOnWriteArrayList();
+        for(int i = 0; i < nTasks; i++){
+            task_flag.add(false);
+        }
 
+        try {
+            boolean finish_flag = false;
+            while(!finish_flag){
+                finish_flag = true;
+                for(int i = 0; i < nTasks; i++){
+                    if(task_flag.get(i))
+                        continue;
+                    finish_flag = false;
+
+                    DoTaskArgs taskArgs = new DoTaskArgs(jobName, mapFiles[i], phase, i, nOther);
+                    Task t = new Task(registerChan.read(), downLatch, registerChan, task_flag, taskArgs);
+                    Thread do_task = new Thread(t);
+                    do_task.start();
+                }
+                //wait for all task have completed, then return
+                downLatch.await();
+            }
+        }
+        catch (InterruptedException i){
+            i.printStackTrace();
+        }
         System.out.println(String.format("Schedule: %s done", phase));
+    }
+
+    //my task thread
+    private static class  Task implements Runnable{
+        private String worker;
+        private CountDownLatch downLatch;
+        private Channel<String> registerChan;
+        private DoTaskArgs taskArgs;
+        private CopyOnWriteArrayList<Boolean> task_flag;
+        Task(String worker, CountDownLatch downLatch, Channel<String> registerChan, CopyOnWriteArrayList<Boolean> task_flag, DoTaskArgs taskArgs){
+            this.downLatch = downLatch;
+            this.registerChan = registerChan;
+            this.worker = worker;
+            this.taskArgs = taskArgs;
+            this.task_flag = task_flag;
+        }
+
+        @Override
+        public  void  run(){
+            //run task
+            Call.getWorkerRpcService(this.worker).doTask(this.taskArgs);
+            this.downLatch.countDown();
+            //mark the task done
+            this.task_flag.set(this.taskArgs.taskNum, true);
+            try {
+                registerChan.write(this.worker);
+            }
+            catch (InterruptedException i){
+                i.printStackTrace();
+            }
+        }
     }
 }
